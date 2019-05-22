7d2644a2843465046e8ecd09aa185f6dc1da8b08
ibilalbashir <ibilalbashir@gmail.com>
2019-05-22 12:49:01 +0500

    invite link

--- common/models/invitations.js
+++ common/models/invitations.js
@@ -12,10 +12,12 @@ module.exports = function (Invitations) {
       text: 'my text',
       html: '<h1>Thank You</h1><p>Thanks for registering.Please follow the link below to complete your registration.</p><p><a href="http://localhost:4200/earlyaccess">Click here to verify email</a></p>'
     }, function (err, mail) {
-      context.res.status(200).send(userInstance)
-      console.log('email sent!');
+      if (!err) {
+        context.res.status(200).send(userInstance)
+        console.log('email sent!' + mail);
+      }
       if (err) {
-        console.log(err);
+        console.log('error is', err);
 
       }
       ;
