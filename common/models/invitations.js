a97a826b28398ef5a909229375c56268879bc641
ibilalbashir <ibilalbashir@gmail.com>
2019-05-27 01:45:18 +0500

    finished it

--- common/models/invitations.js
+++ common/models/invitations.js
@@ -3,30 +3,30 @@
 module.exports = function (Invitations) {
   Invitations.validatesUniquenessOf('email', { message: 'Email already exists' });
 
-  // Invitations.afterRemote("create", function (context, userInstance, next) {
-  //   console.log('after remote is called', userInstance.email)
+  Invitations.afterRemote("create", function (context, userInstance, next) {
+    console.log('after remote is called', userInstance.email)
 
-  //   let res = context.res;
+    let res = context.res;
 
 
-  // Invitations.app.models.Email.send({
-  //   to: userInstance.email,
-  //   from: 'info@tesseractdev.xyz',
-  //   subject: 'Invitation email',
-  //   text: 'my text',
-  //   html: '<h1>Thank You</h1><p>Thanks for registering.Please follow the link below to complete your registration.</p><p><a href="http://localhost:4200/earlyaccess">Click here to verify email</a></p>'
-  // }, function (err, mail) {
-  //   if (!err) {
-  //     context.res.status(200).send(userInstance)
-  //     console.log('email sent!' + mail);
-  //   }
-  //   if (err) {
-  //     console.log('error is', err);
+    Invitations.app.models.Email.send({
+      to: userInstance.email,
+      from: 'shah.khokhar@hotmail.com',
+      subject: 'Invitation email',
+      text: 'my text',
+      html: `<h1>Thank You</h1><p>Thanks for registering.Please follow the link below to complete your registration.</p><p><a href="http://localhost:4200/earlyaccess?id=${userInstance.id}">Click here to verify email </a></p>`
+    }, function (err, mail) {
+      if (!err) {
+        context.res.status(200).send(userInstance)
+        console.log('email sent!' + mail);
+      }
+      if (err) {
+        console.log('error is', err);
 
-  //   }
-  //   ;
-  // })
+      }
+      ;
+    })
 
-  //});
+  });
 
 };
